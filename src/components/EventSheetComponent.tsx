d315a056ca58606ddc7b66496f45ae40a3a50ddd
chamila.ranansinghe@gmail.com <chamila.ranansinghe@gmail.com>
2026-03-25 15:50:47 -0700

    removed days

--- src/components/EventSheetComponent.tsx
+++ src/components/EventSheetComponent.tsx
@@ -7,7 +7,7 @@ import {
 } from "../classes/CalendarFunctions";
 import { XIcon } from "../icons/CalenderIcons";
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import dayjs, { Dayjs } from 'dayjs';
+import dayjs from 'dayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import "../css/EventSheetComponent.scss";
