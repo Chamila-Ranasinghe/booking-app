66407d6e387c2d0e8170f60a9e2fa3f20029e504
chamila.ranansinghe@gmail.com <chamila.ranansinghe@gmail.com>
2026-04-01 18:05:28 -0700

    api call change

--- src/components/RegisterComponent.tsx
+++ src/components/RegisterComponent.tsx
@@ -3,7 +3,7 @@ import { useState, type FC, type FormEvent, type ChangeEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import {UserIcon, EmailIcon, LockIcon, PhoneIcon, EyeIcon, CheckIcon, AlertIcon, EmailCheckIcon} from "../icons/RegisterIcons";
 import type {FormState, FormErrors} from "../classes/RegisterClass";
-import { postData } from "../api/common";
+import { postData, useApiMutation } from "../api/common";
 import { createUser} from "../api/APIclass";
 
 /* ══════════════════════════════════════════════════════════════
@@ -66,8 +66,8 @@ const Register: FC = () => {
     if (!validate()) return;
     setLoading(true);
     await new Promise(r => setTimeout(r, 1600));
-    // const responce = useApiMutation((form)=> postData(createUser, form))
-    const responce = postData(createUser, form);
+    const responce = useApiMutation((form)=> postData(createUser, form))
+    // const responce = postData(createUser, form);
     console.log(responce);
     setLoading(false);
     setSuccess(true);
