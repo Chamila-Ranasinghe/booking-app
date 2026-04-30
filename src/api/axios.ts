459633c5fe05e3ad837950cea3a9ce3a89b49c09
chamila.ranansinghe@gmail.com <chamila.ranansinghe@gmail.com>
2026-04-30 14:20:53 +0530

    position changes

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
+  withCredentials: true,
 });
 
 // Attach token automatically
 api.interceptors.request.use((config) => {
-  config.withCredentials = true;
   const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
